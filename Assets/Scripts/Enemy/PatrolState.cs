2446038285b5e05ddfc30ba963aa0f2111d8b795
“niepandou” <niepandou@163.com>
2025-05-25 00:20:17 +0800

    追击状态机补充

---
+++ Assets/Scripts/Enemy/PatrolState.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utils;
+
+public class PatrolState : BaseState
+{
+    public override void OnEnter(Enemy enemy)
+    {
+        currentEnemy = enemy;
+        currentEnemy.currentSpeed = currentEnemy.patrolSpeed;
+    }
+
+    public override void LogicUpdate()
+    {
+        //发现玩家
+        if (currentEnemy.FoundPlayer())
+        {
+            currentEnemy.SwitchState(NpcState.Chase);
+        }
+        //巡逻逻辑
+        
+        //撞墙
+        if (currentEnemy.physicsCheck.isOnEdge || currentEnemy.physicsCheck.headTouchCheck)
+        {
+            currentEnemy.isWait = true;
+        }
+    }
+
+    public override void PhysicsUpdate()
+    {
+        
+    }
+
+    public override void OnExit()
+    {
+        
+    }
+}
